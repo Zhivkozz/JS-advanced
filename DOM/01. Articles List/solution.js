6ccefdbf8c49e7f68e41e2a919d70f06a780f1f2
zhivko85 <35852893+zhivko85@users.noreply.github.com>
2020-01-12 12:14:34 +0200

    dom1

--- DOM/01. Articles List/solution.js
+++ DOM/01. Articles List/solution.js
@@ -1,4 +1,17 @@
 function createArticle() {
+	let article = document.createElement("article")
+	let h3 = document.createElement("h3");
+	let p = document.createElement("p");
+	let imput = document.getElementById("createTitle");
+	let textarea = document.getElementById("createContent");
+	let articles =   document.getElementById("articles");
+
+	h3.innerHTML = imput.value
+	p.innerHTML = textarea.value
+	article.appendChild(h3);
+	article.appendChild(p);
 	
-	//TODO...
+    articles.appendChild(article)
+
+
 }
