75125ebbcb27857fba252beb20dc324b59771ecf
Zhivko Ivanov <zhivko.ivanov@braingroup.ch>
2020-02-02 17:44:29 +0200

    dom man 4

--- DOM manipulations/04. Sections/script.js
+++ DOM manipulations/04. Sections/script.js
@@ -1,19 +1,25 @@
 function create(words) {
-   // You will receive an array of strings.
-   //  For each string, create a div with a paragraph 
-   //  with the string in it.
-   //   Each paragraph is initially hidden 
-   //   (display:none). Add a click event listener to each div
-   //    that displays the hidden paragraph. 
-   //    Finally, you should append all divs to the element
-   //     with an id "content".
-
-   console.log()
-   
-for (let el of words) {
-   let div = document.createElement ("div")
-   let p = document.createElement("p")
-   
+  // You will receive an array of strings.
+  //  For each string, create a div with a paragraph
+  //  with the string in it.
+  //   Each paragraph is initially hidden
+  //   (display:none). Add a click event listener to each div
+  //    that displays the hidden paragraph.
+  //    Finally, you should append all divs to the element
+  //     with an id "content".
+  const content = document.getElementById("content");
+  console.log();
+  for (let el of words) {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.innerHTML = el;
+    p.style.display = "none";
+     div.appendChild (p)
+content.appendChild(div)
+    div.addEventListener("click", function (e) {
+      e.preventDefault(); 
+   p.style.display = "block"
+   })
+  }
 }
-
-}
+//100/100
