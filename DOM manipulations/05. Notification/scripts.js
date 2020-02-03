5a2b8b786562fccf5577a92684648d908bc9c6ab
Zhivko Ivanov <zhivko.ivanov@braingroup.ch>
2020-02-03 09:20:47 +0200

    a

--- DOM manipulations/05. Notification/scripts.js
+++ DOM manipulations/05. Notification/scripts.js
@@ -1,3 +1,10 @@
 function notify(message) {
-    console.log('TODO:...');
+  // console.log("dddddddddd");
+  let not = document.getElementById("notification");
+  not.innerHTML=(message);
+  not.style.display = "block";
+  setTimeout(function () {
+    not.style.display = "none";
+}, 2000);
 }
+//100/100
